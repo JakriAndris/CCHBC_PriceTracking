6e987899c1f00d774c95a62c421278048a70078a
Jákri András <andrasjakri@gmail.com>
2024-11-17 15:54:04 +0100

    CPL mapping added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data_handling import load_excel, get_price, save_excel, shift_weeks, compar
 
 
 df = load_excel('pricetracking_auto.xlsx', 'auto')
-#shift_weeks(df)
-#get_price(df)
+shift_weeks(df)
+get_price(df)
 compare_to_last_week(df)
 save_excel(df, 'pricetracking_auto.xlsx', 'auto')
--- webscraper.py
+++ webscraper.py
@@ -4,7 +4,28 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 def scraper(cpl, uid):
-    url_base = 'https://auchan.hu/.p-{}'
+    cpl_mapping = {
+        "Auchan": {
+            "url_base": "https://auchan.hu/.p-{}",
+            "xpath": "//span[@class='FBX6R-Sn']",
+        },
+        "Tesco": {
+            "url_base": "https://bevasarlas.tesco.hu/groceries/hu-HU/products/{}",
+            "xpath": "//span[@data-auto='price-value']",
+        },
+        "Spar": {
+            "url_base": "https://www.spar.hu/onlineshop/p/{}",
+            "xpath": "//label[@data-baseprice]",
+        },
+    }
+
+    cpl_config = cpl_mapping.get(cpl)
+    if not cpl_config:
+        raise ValueError(f"Unsupported CPL: {cpl}")
+
+    url_base = cpl_config["url_base"]
+    xpath = cpl_config["xpath"]
+
     url = url_base.format(uid)
 
     driver = webdriver.Chrome()
@@ -12,7 +33,7 @@ def scraper(cpl, uid):
     try:
         driver.get(url)
         driver.implicitly_wait(5)
-        price_element = driver.find_element(By.XPATH, "//span[@class='FBX6R-Sn']")
+        price_element = driver.find_element(By.XPATH, xpath)
         price_element = price_element.text
     except NoSuchElementException:
         print(f"ELement not found for: {uid}")
